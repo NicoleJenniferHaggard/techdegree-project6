882794b5849b7e615051eb85082f81745e6e4a97
NicoleJenniferHaggard <nicolejenniferhaggard8@gmail.com>
2022-03-24 10:29:39 -0700

    added js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const overlay = document.getElementById("overlay");
 const title = document.querySelector('.title');
 const ul = document.querySelector('ul');
 
-let missedGuesses = 0;
+let missedGuess = 0;
 
 // random phrases
 let phrases = [
@@ -20,3 +20,40 @@ let phrases = [
 startGame.addEventListener('click', () => {
   overlay.style.display = 'none';
 });
+
+//return a random phrase from array
+function getRandomPhraseAsArray(arr) {
+  const randomNumber = Math.floor(Math.random()*arrlength);
+  const randomPhrase = arr[randomNumber].toLowerCase();
+  let randomSplit = randomPhrase.split(' ');
+  console.log(randomSplit);
+  return randomPhrase.split(' ');
+}
+
+// adds the letters of a string to the display
+function addPhraseToDisplay (arr) {
+   for (i = 0; i < splitPhrase.length; i++) {
+     const li = document.createElement('li');
+       li.textContent = splitPhrase[i];
+       ul.appendChild(li);
+        //checking for spacing
+        if (splitPhrase[i] === " " ) {
+  text.className.add('space');
+} else {
+    li.className.add('letter');
+}
+}
+};
+
+//check the letters of a string to the display
+function checkLetter(button) {
+  const li = document.querySelectorAll('li');
+  let match = null;
+    for (i = 0; i < li.length; i++) {
+      if (li[i].textContent === li[i].textContent.toLowerCase() ) {
+      li[i].className.add('show');
+      match = (li[i] = button.textContent);
+    }
+  }
+  return match;
+};
