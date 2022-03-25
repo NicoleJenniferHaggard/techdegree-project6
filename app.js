54fcd2d1d55f41abae46ae937edff7332f485d6a
NicoleJenniferHaggard <nicolejenniferhaggard8@gmail.com>
2022-03-25 14:55:20 -0700

    display

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const phrase = document.getElementById('phrase');
 const startGame = document.querySelector('.btn__reset');
 const overlay = document.getElementById("overlay");
 const title = document.querySelector('.title');
-const ul = document.querySelector('ul');
+const phraseUl = document.querySelector('#phrase ul');
 
 let missedGuess = 0;
 
@@ -23,65 +23,77 @@ startGame.addEventListener('click', () => {
 
 //return a random phrase from array
 function getRandomPhraseAsArray(arr) {
-  const randomPhrase = phraseArray[Math.floor(Math.random() * phraseArray.length)];
+  const randomPhrase = arr[Math.floor(Math.random() * arr.length)];
   let phraseSplit = randomPhrase.split('');
-  console.log(randomSplit);
-  return randomSplit;
+  console.log(phraseSplit);
+  return phraseSplit;
 }
+
+const randomPhrasesArray = getRandomPhraseAsArray(phrases);
+
 
 // adds the letters of a string to the display
 function addPhraseToDisplay(arr) {
-  const li = document.querySelectorAll('li');
-  let match = null;
-  for(let i = 0; i < li.length; i++){
-    if (button.textContent === li[i].textContent){
-      li[i].classList.add('show');
-      match = button.textContent;
+  for (let i = 0; i <arr.length; i += 1){
+    const li = document.createElement('li');
+    phraseUl.appendChild(li);
+    if (arr[i] !== ' ') {
+      li.className = 'letter';
+    } else {
+      li.className = 'space';
     }
   }
-}
+  addPhraseToDisplay(randomPhraseArray);
+
+
+
+
+
+
+
+  
 //check the letters of a string to the display
-function checkLetter(button) {
-  const li = document.querySelectorAll('li');
-  let match = null;
-    for (i = 0; i < li.length; i++) {
-      if (li[i].textContent === button.textContent) {
-      li[i].classList.add('show');
-      match = (li[i] = button.textContent);
-    }
-  }
-  return match;
-};
+// function checkLetter(button) {
+//   const li = document.querySelectorAll('li');
+//   let match = null;
+//     for (i = 0; i < li.length; i++) {
+//       if (li[i].textContent === button.textContent) {
+//       li[i].classList.add('show');
+//       match = (li[i] = button.textContent);
+//     }
+//   }
+//   return match;
+// };
 
 // listens for the on screen keyboard to be clicked
-qwerty.addEventListener('click', (e) => {
-  let buttonClick = e.target;
-  if (buttonClick.tagName === 'BUTTON' || button.className === 'chosen') {
-    buttonClick.className === 'chosen';
-    buttonClick.disabled = true;
-    const letterFound = checkLetter(e.target);
-  if (letterFound === null) {
-    const heartLoss = document.querySelectorAll(".tries img");
-    heartLoss.src='images/lostHeart.png';
-    heartLoss.className = 'lost';
-    missedGuess++;
-  }
-  }
-  checkWin();
-});
+// qwerty.addEventListener('click', (e) => {
+//   let buttonClick = e.target;
+//   if (buttonClick.tagName === 'BUTTON' || button.className === 'chosen') {
+//     buttonClick.className === 'chosen';
+//     buttonClick.disabled = true;
+//     const letterFound = checkLetter(e.target);
+//   if (letterFound === null) {
+//     const heartLoss = document.querySelectorAll(".tries img");
+//     heartLoss.src='images/lostHeart.png';
+//     heartLoss.className = 'lost';
+//     missedGuess++;
+//   }
+//   }
+//   checkWin();
+// });
 
 //checks if the game has been wont or lost
-function checkWin() {
-  const letter = document.querySelectorAll('.letter');
-  const show = document.querySelectorAll('show');
-  if (letter.length === show.letter.length) {
-    overlay.className = 'win';
-    title.textContent = 'You Win!';
-    overlay.style.display = 'flex';
-    }
-    if (missedGuess > 4) {
-      overlay.className = 'lose';
-      title.textContent = 'You Lose';
-      overlay.style.dispay = 'flex';
-    }
+// function checkWin() {
+//   const letter = document.querySelectorAll('.letter');
+//   const show = document.querySelectorAll('show');
+//   if (letter.length === show.letter.length) {
+//     overlay.className = 'win';
+//     title.textContent = 'You Win!';
+//     overlay.style.display = 'flex';
+//     }
+//     if (missedGuess > 4) {
+//       overlay.className = 'lose';
+//       title.textContent = 'You Lose';
+//       overlay.style.dispay = 'flex';
+//     }
 };
